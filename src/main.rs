c8f2263a7296ce8c98e5e26c4e526e32fddec076
Dustin Brown <dbrown9@gmail.com>
2021-10-17 17:09:54 -0700

    Toturial code and env setup in one commit!
    This is what happens when you don't start off with git initiated.

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std::collections::HashMap;
+use std::env::args;
+
+fn main() {
+    let action = args().nth(1).expect("Please specify an action");
+    let item = args().nth(2).expect("Please specify an item");
+
+    let mut todo = Todo::new().expect("Initialization of db failed");
+    if action == "add" {
+        todo.insert(item);
+        match todo.save() {
+            Ok(_) => println!("todo saved"),
+            Err(why) => println!("An error occurred: {}", why),
+        }
+    } else if action == "complete" {
+        match todo.complete(&item) {
+            None => println!("{} not found in the list", item),
+            Some(_) => match todo.save() {
+                Ok(_) => println!("'{}' complete", &item),
+                Err(why) => println!("An error occurred: {}", why),
+            },
+        }
+    }
+}
+
+struct Todo {
+    map: HashMap<String, bool>,
+}
+
+impl Todo {
+    fn new() -> Result<Todo, std::io::Error> {
+        let file = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.json")?;
+
+        match serde_json::from_reader(file) {
+            Ok(map) => Ok(Todo { map }),
+            Err(e) if e.is_eof() => Ok(Todo {
+                map: HashMap::new(),
+            }),
+            Err(e) => panic!("An error occurred: {}", e),
+        }
+    }
+
+    fn insert(&mut self, key: String) {
+        self.map.insert(key, true);
+    }
+
+    fn complete(&mut self, key: &String) -> Option<()> {
+        match self.map.get_mut(key) {
+            Some(value) => Some(*value = false),
+            None => None,
+        }
+    }
+
+    fn save(self) -> Result<(), Box<dyn std::error::Error>> {
+        let file = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("db.json")?;
+
+        serde_json::to_writer_pretty(file, &self.map)?;
+        Ok(())
+    }
+}
